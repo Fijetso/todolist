f99b177b62fc84b66a9f78380f4ce9701b5aa76b
Fijetso <henrynguyen02081997@gmail.com>
2019-05-26 21:32:41 +0700

    Init Components

--- todolist-client/src/app/app.module.ts
+++ todolist-client/src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { Router, Routes, RouterModule } from "@angular/router";
+import { TodoListComponent } from "./todo-list/todo-list.component";
+import { FormsModule } from "@angular/forms";
 
+const appRoutes: Routes = [
+  {
+    path: "todolist",
+    component: TodoListComponent
+  },
+  {
+    path: "",
+    component: TodoListComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    NotFoundComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
